e7ba02686da025b8f9c949a2f22087d0d9ff7f2b
Derek McTavish <derek@mctavishdynamics.com>
2024-11-22 02:34:51 -0700

    Remove unused font and CSS assets
    Deleted various Barlow semi-condensed, Nunito Sans font files, and CSS assets from the `storybook-static` directory. This clean-up aims to reduce clutter and optimize the project by removing unnecessary static assets.

--- storybook-static/assets/Style-CbkTJlvC.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-DR9Q75dM.js";import{useMDXComponents as s}from"./index-ChEI-nsM.js";import"./index-DRjF_FHU.js";function t(e){const n={code:"code",h1:"h1",pre:"pre",...s(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(n.h1,{id:"css-variables",children:"CSS Variables"}),`
-`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-css",children:`--ooee--ComboBox--outline-width--focus-within: 2px;
---ooee--ComboBox--outline-style--focus-within: solid;
---ooee--ComboBox--outline-color--focus-within: #aaa;
---ooee--ComboBox--outline-offset--focus-within: 2px;
-`})})]})}function a(e={}){const{wrapper:n}={...s(),...e.components};return n?o.jsx(n,{...e,children:o.jsx(t,{...e})}):t(e)}export{a as default};
